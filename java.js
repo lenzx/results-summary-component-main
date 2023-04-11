084a8537a1dce1181962288d775ff0cc060d4c92
rivaldo <leo_1998@live.cl>
2023-04-10 21:14:41 -0400

    java

--- java.js
+++ java.js
@@ -4,9 +4,24 @@ function traerDatos () {
     xhtpp.send();
     xhtpp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            let datos = JSON.parse(this.responseText)
+            let datos = JSON.parse(this.responseText);
 
-            
+            let res = document.querySelector('.container-elementos');
+            res.innerHTML='';
+            console.log(datos   )
+
+            for (let item of datos) {
+                console.log(item)
+                res.innerHTML += `
+                <div class="elemento">
+                    <div class="image"><img src="${item.icon}" alt="reaction"></div>
+                    <p class="title-sumary" >${item.category}</p>
+                    <p class="sumary-p" >${item.score}</p>
+                    <p class="sumary-p2" ">/ 100</p>
+                </div>
+                `
+            }   
         }
     }
 }
+traerDatos()
