c74f97e2591e2d207c782952ede6a355e53398d4
rivaldo <leo_1998@live.cl>
2023-04-20 13:18:32 -0400

    commit

--- java.js
+++ java.js
@@ -7,11 +7,10 @@ function traerDatos () {
             let datos = JSON.parse(this.responseText);
 
             let res = document.querySelector('.container-elementos');
-            res.innerHTML='';
-            console.log(datos   )
-
+            console.log('resbefore:',res)
             for (let item of datos) {
-                console.log(item)
+                console.log('item:',item)
+                console.log('res:',res)
                 res.innerHTML += `
                 <div class="elemento">
                     <div class="image"><img src="${item.icon}" alt="reaction"></div>
